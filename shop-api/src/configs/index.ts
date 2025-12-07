d7c11919f1a07da465c4ee3a347b75515f6c19f3
Julian <minkkgkyaw9899@gmail.com>
2025-12-07 10:28:58 +0630

    feat(web-client): start creating web client app

--- shop-api/src/configs/index.ts
+++
@@ -1,5 +0,0 @@
-export default {
-	port: process.env.PORT ?? 3000,
-	jwtSecret: process.env.JWT_SECRET ?? "jwt secrets",
-	dbUrl: process.env.DATABASE_URL ?? "db_url",
-};
--- shop-api/src/generated/prismabox/barrel.ts
+++
@@ -1,4 +0,0 @@
-export * from "./UserGender";
-export * from "./User";
-export * from "./__nullable__";
-export * from "./__transformDate__";
